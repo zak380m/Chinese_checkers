a24b291e7296c20621a10afa3d5c0b8144069a11
zak380m <zak380m@gmail.com>
2024-12-06 22:47:19 +0100

    Creating maven project

---
+++ Chinese_checkers/src/main/java/zak380mGazyli/ClassicBoard.java
@@ -0,0 +1,8 @@
+package Chinese_checkers.src.main.java.zak380mGazyli;
+
+public class ClassicBoard implements Board {
+    @Override
+    public void display() {
+        System.out.println("Classic board");
+    }
+}
