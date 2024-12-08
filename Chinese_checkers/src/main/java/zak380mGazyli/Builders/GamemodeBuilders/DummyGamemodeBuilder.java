ee3c3eaddc79d6c859f86d99355b4081348653e8
Gazyli <bazylinator@gmail.com>
2024-12-08 21:35:07 +0100

    Added DummyGamemodeBuilder

---
+++ Chinese_checkers/src/main/java/zak380mGazyli/Builders/GamemodeBuilders/DummyGamemodeBuilder.java
@@ -0,0 +1,15 @@
+package zak380mGazyli.Builders.GamemodeBuilders;
+
+import zak380mGazyli.Gamemodes.Gamemode;
+
+public class DummyGamemodeBuilder implements GamemodeBuilder {
+    @Override
+    public void buildGamemode() {
+
+    }
+
+    @Override
+    public Gamemode getGamemode() {
+        return null;
+    }
+}
