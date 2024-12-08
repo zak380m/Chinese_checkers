f161541913df500e9b2cf85fc7b1d78966e622dc
Michał <zak380m@gmail.com>
2024-12-08 20:23:21 +0100

    Delete Chinese_checkers/src/main/java/zak380mGazyli/GameModes directory
    deleting GameModes

--- Chinese_checkers/src/main/java/zak380mGazyli/GameModes/GameMode.java
+++
@@ -1,9 +0,0 @@
-package zak380mGazyli.GameModes;
-
-import zak380mGazyli.Boards.*;
-
-public interface GameMode {
-    boolean validateMove(int[] startPos, int[] endPos, Board board);  
-    void processMove(int[] startPos, int[] endPos, Board board);  
-}
-
