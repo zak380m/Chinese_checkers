8c5d46031127379409687f9075fa8973eb5a1d23
Gazyli <bazylinator@gmail.com>
2024-12-11 17:08:27 +0100

    basic board done

--- Chinese_checkers/src/main/java/zak380mGazyli/Cells/Cell.java
+++ Chinese_checkers/src/main/java/zak380mGazyli/Cells/Cell.java
@@ -3,13 +3,27 @@ package zak380mGazyli.Cells;
 import zak380mGazyli.Misc.Color;
 
 public class Cell {
-    private String colorName = Color.WHITE;
+    private String cellSymbol;
+    private String cellColor;
 
-    public void setColor(String colorName) {
-        this.colorName = colorName;
+    public Cell(String cellSymbol, String cellColor) {
+        this.cellSymbol = cellSymbol;
+        this.cellColor = cellColor;
+    }
+
+    public void setSymbol(String cellSymbol) {
+        this.cellSymbol = cellSymbol;
+    }
+
+    public String getSymbol() {
+        return this.cellSymbol;
+    }
+
+    public void setColor(String cellColor) {
+        this.cellColor = cellColor;
     }
 
     public String getColor() {
-        return this.colorName;
+        return this.cellColor;
     }
 }
