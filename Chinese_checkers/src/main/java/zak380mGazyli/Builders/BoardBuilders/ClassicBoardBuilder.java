534ed7b38ef1d8a1f861de6954e1a7989ef5e4d0
Gazyli <bazylinator@gmail.com>
2024-12-11 23:33:36 +0100

    Board setup - coloring and setting pawns done

--- Chinese_checkers/src/main/java/zak380mGazyli/Builders/BoardBuilders/ClassicBoardBuilder.java
+++ Chinese_checkers/src/main/java/zak380mGazyli/Builders/BoardBuilders/ClassicBoardBuilder.java
@@ -2,22 +2,37 @@ package zak380mGazyli.Builders.BoardBuilders;
 
 import zak380mGazyli.Boards.Board;
 import zak380mGazyli.Boards.ClassicBoard;
+import zak380mGazyli.Misc.Color;
 
 public class ClassicBoardBuilder implements BoardBuilder {
-    private Board board;
+    private ClassicBoard board;
 
     @Override
     public void buildBoard(int players) {
         this.board = new ClassicBoard();
         switch (players) {
             case 2:
-                // Logic for creating boards for different player amounts
+                board.colorTriangle2P(1, Color.RED);
+                board.colorTriangle2P(4, Color.BLUE);
                 break;
             case 3:
+                board.colorTriangle(1, Color.RED);
+                board.colorTriangle(3, Color.BLUE);
+                board.colorTriangle(5, Color.GREEN);
                 break;
             case 4:
+                board.colorTriangle(2, Color.RED);
+                board.colorTriangle(3, Color.YELLOW);
+                board.colorTriangle(5, Color.BLUE);
+                board.colorTriangle(6, Color.GREEN);
                 break;
             case 6:
+                board.colorTriangle(1, Color.RED);
+                board.colorTriangle(2, Color.MAGENTA);
+                board.colorTriangle(3, Color.YELLOW);
+                board.colorTriangle(4, Color.BLUE);
+                board.colorTriangle(5, Color.GREEN);
+                board.colorTriangle(6, Color.GREY);
                 break;
             default:
         }
--- Chinese_checkers/src/test/java/zak380mGazyli/ClassicBoardTest.java
+++ Chinese_checkers/src/test/java/zak380mGazyli/ClassicBoardTest.java
@@ -1,5 +1,26 @@
 package zak380mGazyli;
 
+import org.junit.jupiter.api.Test;
+import zak380mGazyli.Boards.Board;
+import zak380mGazyli.Builders.BoardBuilders.BoardBuilder;
+import zak380mGazyli.Builders.BoardBuilders.ClassicBoardBuilder;
+
 public class ClassicBoardTest {
-    
+//    @Test
+//    public void testBoardDisplay() {
+//        BoardBuilder bb = new ClassicBoardBuilder();
+//        bb.buildBoard(2);
+//        Board board = bb.getBoard();
+//        board.displayBoard();
+//    }
+
+    @Test
+    public void testPlayerSetup() {
+        for (int player_count : new int[]{2,3,4,6}) {
+            BoardBuilder bb = new ClassicBoardBuilder();
+            bb.buildBoard(player_count);
+            Board board = bb.getBoard();
+            board.displayBoard();
+        }
+    }
 }
