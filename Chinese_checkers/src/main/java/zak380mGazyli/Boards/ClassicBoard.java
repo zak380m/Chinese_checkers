cbb3ba86f4e6ab0ae46867d29d933bd43e60cd10
Gazyli <bazylinator@gmail.com>
2025-01-09 12:15:28 +0100

    Added getStartArea to Board and ClassicBoard

--- Chinese_checkers/src/main/java/zak380mGazyli/Boards/ClassicBoard.java
+++ Chinese_checkers/src/main/java/zak380mGazyli/Boards/ClassicBoard.java
@@ -27,9 +27,17 @@ public class ClassicBoard implements Board {
         {new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(symbol, Color.WHITE), new Cell(" ", Color.WHITE), new Cell(symbol, Color.WHITE), new Cell(" ", Color.WHITE), new Cell(symbol, Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE)},
         {new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(symbol, Color.WHITE), new Cell(" ", Color.WHITE), new Cell(symbol, Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE)},
         {new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(symbol, Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE), new Cell(" ", Color.WHITE)}
-};
+    };
+
+    private final Map<Integer, Cell[]> triangles = new HashMap<>();
 
     public ClassicBoard() {
+        triangles.put(1, new Cell[]{cells[0][12], cells[1][11], cells[1][13], cells[2][10], cells[2][12], cells[2][14], cells[3][9], cells[3][11], cells[3][13], cells[3][15]});
+        triangles.put(2, new Cell[]{cells[4][18], cells[4][20], cells[4][22], cells[4][24], cells[5][19], cells[5][21], cells[5][23],  cells[6][20], cells[6][22], cells[7][21]});
+        triangles.put(3, new Cell[]{cells[12][18], cells[12][20], cells[12][22], cells[12][24], cells[11][19], cells[11][21], cells[11][23],  cells[10][20], cells[10][22], cells[9][21]});
+        triangles.put(4, new Cell[]{cells[16][12], cells[15][11], cells[15][13], cells[14][10], cells[14][12], cells[14][14], cells[13][9], cells[13][11], cells[13][13], cells[13][15]});
+        triangles.put(5, new Cell[]{cells[12][0], cells[12][2], cells[12][4], cells[12][6], cells[11][1], cells[11][3], cells[11][5],  cells[10][2], cells[10][4], cells[9][3]});
+        triangles.put(6, new Cell[]{cells[4][0], cells[4][2], cells[4][4], cells[4][6], cells[5][1], cells[5][3], cells[5][5],  cells[6][2], cells[6][4], cells[7][3]});
     }
 
     @Override
@@ -77,15 +85,12 @@ public class ClassicBoard implements Board {
         }
     }
 
-    public void colorTriangle(int triangle_num, String color) {
-        Map<Integer, Cell[]> triangles = new HashMap<>();
-        triangles.put(1, new Cell[]{cells[0][12], cells[1][11], cells[1][13], cells[2][10], cells[2][12], cells[2][14], cells[3][9], cells[3][11], cells[3][13], cells[3][15]});
-        triangles.put(2, new Cell[]{cells[4][18], cells[4][20], cells[4][22], cells[4][24], cells[5][19], cells[5][21], cells[5][23],  cells[6][20], cells[6][22], cells[7][21]});
-        triangles.put(3, new Cell[]{cells[12][18], cells[12][20], cells[12][22], cells[12][24], cells[11][19], cells[11][21], cells[11][23],  cells[10][20], cells[10][22], cells[9][21]});
-        triangles.put(4, new Cell[]{cells[16][12], cells[15][11], cells[15][13], cells[14][10], cells[14][12], cells[14][14], cells[13][9], cells[13][11], cells[13][13], cells[13][15]});
-        triangles.put(5, new Cell[]{cells[12][0], cells[12][2], cells[12][4], cells[12][6], cells[11][1], cells[11][3], cells[11][5],  cells[10][2], cells[10][4], cells[9][3]});
-        triangles.put(6, new Cell[]{cells[4][0], cells[4][2], cells[4][4], cells[4][6], cells[5][1], cells[5][3], cells[5][5],  cells[6][2], cells[6][4], cells[7][3]});
+    @Override
+    public Cell[] getStartArea(int area_num) {
+        return triangles.get(area_num);
+    }
 
+    public void colorTriangle(int triangle_num, String color) {
         for (Cell cell : triangles.get(triangle_num)) {
             cell.setSymbol("O");
             cell.setColor(color);
