2bc93ca2cf52807d5a121953c8928106e548c3ae
Gazyli <bazylinator@gmail.com>
2025-01-09 12:15:15 +0100

    Added getStartArea to Board and ClassicBoard

--- Chinese_checkers/src/main/java/zak380mGazyli/Boards/Board.java
+++ Chinese_checkers/src/main/java/zak380mGazyli/Boards/Board.java
@@ -4,6 +4,7 @@ import zak380mGazyli.Misc.Cell;
 
 public interface Board {
     Cell[][] getBoard();
+    Cell[] getStartArea(int area_num);
     Cell[] getNeighbours(int x, int y);
     void updateBoard(int startX, int startY, int endX, int endY);
     void displayBoard();
