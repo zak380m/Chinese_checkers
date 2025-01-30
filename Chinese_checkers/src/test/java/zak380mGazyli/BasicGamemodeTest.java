e2133835db51c219331cb9eacb7b2d3af16d6af2
zak380m <zak380m@gmail.com>
2025-01-30 03:07:53 +0100

    adding test and diagrams

--- Chinese_checkers/src/test/java/zak380mGazyli/BasicGamemodeTest.java
+++ Chinese_checkers/src/test/java/zak380mGazyli/BasicGamemodeTest.java
@@ -24,14 +24,23 @@ public class BasicGamemodeTest {
 
     @Test
     public void testValidateMove() {
-        Gamemode gamemode = new BasicGamemode();
         for (int player_count : new int[]{2,3,4,6}) {
             BoardBuilder bb = new ClassicBoardBuilder();
             bb.buildBoard(player_count);
             Board board = bb.getBoard();
             board.displayBoard();
+            Gamemode gamemode = new BasicGamemode();
             gamemode.setNumberOfPlayers(player_count, board);
-            System.out.println(gamemode.validateMove(9, 3, 10, 4, board));
+            randomMoveTest(gamemode, board);
+            board.displayBoard();
         }
+    }
+
+    private void randomMoveTest(Gamemode gamemode, Board board) {
+        if(gamemode.validateMove(10, 2, 8, 4, board)) gamemode.processMove(10, 2, 8, 4, board);
+        if(gamemode.validateMove(20, 12, 18, 10, board)) gamemode.processMove(20, 12, 18, 10, board);
+        if(gamemode.validateMove(4, 12, 6, 10, board)) gamemode.processMove(4, 12, 6, 10, board);
+        gamemode.processPass();
+        if(gamemode.validateMove(24, 12, 16, 12, board)) gamemode.processMove(24, 12, 16, 12, board);
     }
 }
--- Chinese_checkers/src/test/java/zak380mGazyli/SuperGamemodeTest.java
+++ Chinese_checkers/src/test/java/zak380mGazyli/SuperGamemodeTest.java
@@ -24,14 +24,25 @@ public class SuperGamemodeTest {
 
     @Test
     public void testValidateMove() {
-        Gamemode gamemode = new SuperGamemode();
         for (int player_count : new int[]{2,3,4,6}) {
             BoardBuilder bb = new ClassicBoardBuilder();
             bb.buildBoard(player_count);
             Board board = bb.getBoard();
             board.displayBoard();
+            Gamemode gamemode = new SuperGamemode();
             gamemode.setNumberOfPlayers(player_count, board);
-            System.out.println(gamemode.validateMove(12, 0, 8, 4, board));
+            randomMoveTest(gamemode, board);
+            board.displayBoard();
         }
+    }
+
+    private void randomMoveTest(Gamemode gamemode, Board board) {
+        if(gamemode.validateMove(12, 0, 16, 4, board)) gamemode.processMove(12, 0, 16, 4, board);
+        if(gamemode.validateMove(23, 5, 8, 4, board)) gamemode.processMove(23, 5, 8, 4, board);
+        if(gamemode.validateMove(24, 12, 12, 0, board)) gamemode.processMove(24, 12, 12, 0, board);
+        if(gamemode.validateMove(12, 16, 23, 5, board)) gamemode.processMove(12, 16, 23, 5, board);
+        if(gamemode.validateMove(0, 12, 24, 12, board)) gamemode.processMove(0, 12, 24, 12, board);
+        if(gamemode.validateMove(0, 4, 12, 16, board)) gamemode.processMove(0, 4, 12, 16, board);
+        if(gamemode.validateMove(11, 1, 14, 4, board)) gamemode.processMove(11, 1, 14, 4, board);
     }
 }
